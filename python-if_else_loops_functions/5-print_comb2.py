765cbcf715bb68653b3b7df8f8142b1302dfd3ef
glenmiracle <m.bonyu@alustudent.come>
2023-07-06 11:30:28 -0700

    done

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i==9 and j==9:
-            print("{}{}".format(i, j), end='')
-        else:
-            print("{}{}, ".format(i, j), end='')
+for i in range(0, 100):
+    if i == 99:
+        print("{:02d}".format(i), end='')
+    else:
+        print("{:02d}, ".format(i), end='')
