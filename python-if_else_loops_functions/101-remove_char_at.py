9b1738ea61b332d53695f7a4d779fdc0c8ce24d1
glenmiracle <m.bonyu@alustudent.come>
2023-07-07 06:11:02 -0700

    done

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+def remove_char_at(str, n):
+    str_copy = str[:]
+    new_str = ""
+    for i in range(len(str_copy)):
+        if i != n:
+            new_str += str_copy[i]
+    return new_str
