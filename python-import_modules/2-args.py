a08a716a3fafe7e7a2157d44c236601be2968aeb
glenmiracle <m.bonyu@alustudent.come>
2023-07-08 05:08:06 -0700

    done

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if count == 0:
         print("0 arguments")
     elif count == 1:
-        print("1 argument: ")
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
     for i in range(count):
