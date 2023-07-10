981caeee99d4f25d90da16344c6390b3ebcc30e0
glenmiracle <m.bonyu@alustudent.come>
2023-07-10 04:07:43 -0700

    done

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    # print(add(20, 3)) # Output:  23
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit_value = 1
-        sys.exit(exit_value)
-    else:
-        arg1 = sys.argv[1]
-        operator = sys.argv[2]
-        arg0 = sys.argv[0]
-        arg3 = sys.argv[3]
+        sys.exit(1)
 
-        ops = ['+', '*', '/', '-']
-        if operator not in ops:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit_value = 1
-            sys.exit(exit_value)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-        if operator == '+':
-            sum  = add(int(arg1), int(arg3))
-            print("{} {} {} = {}".format(arg1, operator, arg3, sum))
-        elif operator == '*':
-            product  = mul(int(arg1), int(arg3))
-            print("{} {} {} = {}".format(arg1, operator, arg3, product))
-
-        elif operator == '-':
-            difference  = sub(int(arg1), int(arg3))
-            print("{} {} {} = {}".format(arg1, operator, arg3, difference))
-
-        elif operator == '/':
-            division  = div(int(arg1), int(arg3))
-            print("{} {} {} = {}".format(arg1, operator, arg3, division))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
