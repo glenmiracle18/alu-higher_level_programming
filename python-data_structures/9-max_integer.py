df99ceefa8bddc63d1edee8225109e3385f182e2
glenmiracle <m.bonyu@alustudent.come>
2023-07-15 08:30:51 -0700

    done

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """Returns maximum integer in list"""
-    max_int = my_list[0]  #Assume the fist element is the highest
-    if len(my_list) == 0:
+    if not my_list:
         return None
-    else:
-        for i in my_list:
-            if i > max_int:
-                max_int = i
-        return max_int
+    max_int = my_list[0] 
+    for i in my_list:
+        if i > max_int:
+            max_int = i
+    return max_int
