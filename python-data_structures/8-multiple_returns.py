bb1d5f92b2922dd909b629fe599aa515b74a5fe0
glenmiracle <m.bonyu@alustudent.come>
2023-07-15 07:44:26 -0700

    doen

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     # return the lenth of the sentence
-    first_char = sentence[0]
-    return(len(sentence), first_char)
+    if len(sentence) == 0:
+        Length = 0
+        first_char = 'None'
+    else:
+        first_char = sentence[0]
+        Length = len(sentence)
+    return(Length, first_char)
