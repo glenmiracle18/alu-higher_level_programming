13c62200d2bc5482bd611b1dce62086d5f0c72cd
glenmiracle <m.bonyu@alustudent.come>
2023-07-18 07:52:14 -0700

    done

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_two(a_dictionary):
+    new_dict = {}
+    for k, v in a_dictionary.items():
+        new_v = v*2
+        new_dict[k] = new_v
+    return new_dict
