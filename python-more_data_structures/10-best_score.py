73bc010e1b841521c8891f51b70b830139e180d3
glenmiracle <m.bonyu@alustudent.come>
2023-07-18 08:46:48 -0700

    done

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-        if not a_dictionary:
-              return None
-        else:
-            values = []
-            for k, v in a_dictionary.items():
-                values.append(v)
-            max_val = max(values)
-            name=''
-            for n, s in a_dictionary.items():
-                    if s == max_val:
-                        name += str(n)
-            return name
+    if not a_dictionary:
+            return None
+    else:
+        values = []
+        for k, v in a_dictionary.items():
+            values.append(v)
+        max_val = max(values)
+        name=''
+        for n, s in a_dictionary.items():
+                if s == max_val:
+                    name += str(n)
+        return name
