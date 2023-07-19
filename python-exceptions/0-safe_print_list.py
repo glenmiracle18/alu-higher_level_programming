77d3a12c3c0e7b113657bacb53a4d798bb6af6e4
glenmiracle <m.bonyu@alustudent.come>
2023-07-19 06:42:43 -0700

    done

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,9 @@ try:
         var = map(str, new_list)
         result_string = ''.join(var)
         result_int = int(result_string)
-        print(result_int)
-        return len(new_list)
+        count = 0
+        for i in new_list:
+            count += 1
+        return count
     except TypeError:
         print("There is a value Error")
