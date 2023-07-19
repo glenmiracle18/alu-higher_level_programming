d10eee232f52ddfd63ce9f27556f29c0cf25c857
glenmiracle <m.bonyu@alustudent.come>
2023-07-19 06:56:50 -0700

    done

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        new_list = []
-        for item in my_list[:x]:
-            new_list.append(item)
-        var = map(str, new_list)
-        result_string = ''.join(var)
-        result_int = int(result_string)
-        print(result_int)
-        count = 0
-        for i in new_list:
-            count += 1
-        return count
-    except TypeError:
-        print("There is a value Error")
+    total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            total += 1
+        except IndexError:
+            break
+    print()
+    return(total)
