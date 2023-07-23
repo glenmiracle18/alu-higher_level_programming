82b7f5c0761ffeb207b9b3a0193c0f15b31cd2c3
glenmiracle <m.bonyu@alustudent.come>
2023-07-23 11:23:10 -0700

    done

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Ehoneah Obed
+"""Defines a square """
+
+
+class Square:
+    """Does nothing"""
+
+    def __init__(self, size):
+        """Initializing the square class
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size
