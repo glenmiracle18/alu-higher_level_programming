24855d760693a4b6cac9d5bc0968ae534866aeb5
glenmiracle <m.bonyu@alustudent.come>
2023-07-25 16:33:22 -0700

    done

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ", ".join(["BestSchool" for i in range(magic_string.count)])
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
