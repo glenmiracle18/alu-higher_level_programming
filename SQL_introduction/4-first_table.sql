e73e391bba3a6a12a6e077e043a536a4d7382e87
glenmiracle <m.bonyu@alustudent.come>
2023-07-31 01:54:11 -0700

    done

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+-- creates a table called first_table
+-- id = INT, name VARCHAR(256)
+-- database name will be passed as an argument
+-- not allowed to use SELECT or SHOW statements
+
+CREATE DATABASE first_table (
+	name INT,
+	name VARCHAR(256),
+);
