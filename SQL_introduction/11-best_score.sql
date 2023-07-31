dcce7d7c887c0b7365ea95618f56f23efdb4c68a
glenmiracle <m.bonyu@alustudent.come>
2023-07-31 04:09:02 -0700

    done

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -2,7 +2,4 @@
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
 
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score;
