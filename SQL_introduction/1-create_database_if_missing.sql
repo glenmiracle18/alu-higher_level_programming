38083c57f27c6add863a637023f84f0064edc394
glenmiracle <m.bonyu@alustudent.come>
2023-07-31 01:33:10 -0700

    done

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- creates a database called hbtn_0c_0 in mysql server
 -- if the database exists, the script should not fail
 
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
