0adaa814d681095d77cc5ae6a64abd0542de203b
glenmiracle <m.bonyu@alustudent.come>
2023-07-31 02:12:53 -0700

    done

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,10 @@
--- creates a table called first_table
--- id = INT, name VARCHAR(256)
--- database name will be passed as an argument
--- not allowed to use SELECT or SHOW statements
+-- creates a table called first_table in the current database in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+-- first_table description:
+--             id INT
+--             name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+-- If the table first_table already exists, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
 
-CREATE TABLE IF NOT EXIST first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
