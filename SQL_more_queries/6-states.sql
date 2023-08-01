59b68f363ed9b069501d04560678d2f450def646
glenmiracle <m.bonyu@alustudent.come>
2023-08-01 14:01:15 -0700

    done

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,4 +2,5 @@
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(256));
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(256));
