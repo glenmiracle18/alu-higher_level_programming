2c171bfa9c6cc888b1a9992f71143ae58307ae46
glenmiracle <m.bonyu@alustudent.come>
2023-08-01 13:58:57 -0700

    done

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(256));
