1055babe93f0a66dffa73869ee7e31b2ef8276ea
glenmiracle <m.bonyu@alustudent.come>
2023-08-01 13:13:21 -0700

    don

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,5 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXIXTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d2_pwd';
-GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
