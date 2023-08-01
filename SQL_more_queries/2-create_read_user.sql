f51f1e34434d6a4ce77f0009e8afb3f6a2bbf61a
glenmiracle <m.bonyu@alustudent.come>
2023-08-01 13:12:22 -0700

    done

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- create a database and a user 'user_0d_2'
+-- user should only have SELECT privileges in the new database
+-- set a user password
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXIXTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d2_pwd';
+GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
