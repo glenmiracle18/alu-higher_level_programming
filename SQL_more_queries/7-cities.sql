8e1f8e8d8be06511c034901c9a874ee75654bac8
glenmiracle <m.bonyu@alustudent.come>
2023-08-01 14:12:33 -0700

    done

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- id must be uniqe, auto incremental, and can't be null and also PK
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERNCES states(id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERNCES hbtn_0d_usa.states(id));
