1af21dfa1e0b7621644581075fe12d7463432486
glenmiracle <m.bonyu@alustudent.come>
2023-08-01 14:09:24 -0700

    done

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- creates a new table in the database hbtn_0d_usa called cities
+-- id must be uniqe, auto incremental, and can't be null and also PK
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERNCES states(id));
