f9b7b8715a1a078e7656e2708d139f5306ca1ee0
glenmiracle18 <m.bonyu@alustudent.com>
2023-08-04 11:09:19 +0000

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+""" module url requests"""
 import urllib.request
 
 
+"""making request to provided url"""
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    """making request with intranet url"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        """ string manipulation technique
+        """
+        temp = str(html)
+        print("\t- utf8 content: {}".format(temp[2:-1]))
