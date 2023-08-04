c7824a6e855c8968256862791abd03fc6c6a7bbb
glenmiracle18 <m.bonyu@alustudent.com>
2023-08-04 11:39:40 +0000

    done

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module url requests"""
+import urllib.request
+import sys
+
+"""making request to provided url"""
+if __name__ == "__main__":
+    """"make a re quest to get the X-Request-Id"""
+    url = sys.argv[1]
+    with urllib.request.urlopen("url") as response:
+        data = response.headers.get('X-Request-Id')
+        print("{}".format(data), end="")
