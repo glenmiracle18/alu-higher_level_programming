1ef25a579b98801b6de47295997f1bb46ba193f0
glenmiracle18 <m.bonyu@alustudent.com>
2023-08-04 10:52:57 +0000

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 """python that fetches a request from a url"""
 import urllib.request
 
-req = urllib.request.Request('https://alu-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    data = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
-    print("\t- utf8 content: {}".format(data.decode("utf-8")))
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alu-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
