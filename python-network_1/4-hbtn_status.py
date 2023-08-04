62991a01cd25f4deddf8aded3116228cc5c9fa65
glenmiracle18 <m.bonyu@alustudent.com>
2023-08-04 13:33:46 +0000

    done: /c \c

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" module url requests"""
+""" module uses the request module
+    to send a request to server """
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
-    print('Body response:')
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    data = r.text
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
