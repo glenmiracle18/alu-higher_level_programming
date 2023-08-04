cde03bae3efa6a639ab5dc191ba515cff6a5f862
glenmiracle18 <m.bonyu@alustudent.com>
2023-08-04 12:46:10 +0000

    done

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module url requests"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read().decode('utf-8')
+            print(data)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code()))
+    except urllib.error.URLError as e:
+        print("URL Error: ".format(e.reason))
