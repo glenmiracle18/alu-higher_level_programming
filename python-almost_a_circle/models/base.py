f3cacd47c322781605311490e28444f24e487121
glenmiracle <m.bonyu@alustudent.come>
2023-08-16 11:42:13 -0700

    done

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+A bases class to be inherited from
+"""
+
+class Base:
+    """
+    A base class
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ initialization """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
+
