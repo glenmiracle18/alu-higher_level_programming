bbc8a8e2d9726ba26f87b66ec768ed04ed2ab5ef
glenmiracle <m.bonyu@alustudent.come>
2023-08-17 13:32:42 -0700

    done

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,2 +1,21 @@
--- Insert states
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+'''a script that lists all State objects from the database hbtn_0e_6_usa
+'''
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}".format(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            pool_pre_ping=True)
+    )
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}")
