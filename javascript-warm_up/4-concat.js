fa9cb065b2388bbd22308214f9ba8b62c0460a23
glenmiracle18 <m.bonyu@alustudent.com>
2023-09-27 20:37:43 +0200

    here

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,8 +3,8 @@ let firstArgument = process.argv[2];
 let secondArgument = process.argv[3];
 if (typeof firstArgument === 'undefined') {
   firstArgument = 'undefined';
-} else if (typeof secondArgument === 'undefined') {
-  secondArgument = 'undefined';
-} else {
-  console.log(firstArgument + ' is ' + secondArgument); // prints out the two arguments passed in
 }
+if (typeof secondArgument === 'undefined') {
+  secondArgument = 'undefined';
+}
+console.log(firstArgument + ' is ' + secondArgument); // prints out the two arguments passed in
