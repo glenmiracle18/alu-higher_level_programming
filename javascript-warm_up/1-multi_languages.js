dbe8dd7b134a731421f9b07fb9ea818cba599664
glenmiracle18 <m.bonyu@alustudent.com>
2023-09-27 19:49:13 +0200

    done

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'JavaScript is amazing';
+console.log(c); // C is fun
+console.log(python); // Python is cool
+console.log(js);// JavaScript is amazing
