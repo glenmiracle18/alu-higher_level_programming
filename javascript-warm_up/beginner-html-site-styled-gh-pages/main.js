39e821af7675eac29a74e7d033522f4bb1715252
glenmiracle18 <m.bonyu@alustudent.com>
2023-09-29 13:38:11 +0200

    done

---
+++ javascript-warm_up/beginner-html-site-styled-gh-pages/main.js
@@ -0,0 +1,24 @@
+// button actions 
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1")
+
+function setUserName () {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozila is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+// function call
+myButton.onclick = () => {
+    setUserName();
+}
+
+
+
---
+++ javascript-warm_up/js_practice/functions.js
@@ -0,0 +1,24 @@
+// const myText = "I am a string";
+// const newString = myText.replace("string", "sausage");
+// console.log(newString);
+
+// const myArray = ["I", "love", "chocolate", "frogs"];
+// const madeAString = myArray.join(" ");
+// console.log(madeAString);
+
+// const myNumber = Math.random();
+// console.log(myNumber);
+
+// function logKey(event) {
+//     console.log(`You pressed "${event.key}".`);
+// }
+
+// textBox.addEventListener("keydown", logKey);
+
+// textBox.addEventListener("keydown", (event) => {
+//     console.log(`You pressed "${event.key}".`);
+// });
+
+const originals = [1, 2, 3];
+const doubled = originals.map(item => item * 2);
+console.log(doubled)
