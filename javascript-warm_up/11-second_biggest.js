89cfe7dcf8ec0ef227d5a56e13303d9e6407a5bf
glenmiracle18 <m.bonyu@alustudent.com>
2023-09-29 15:47:20 +0200

    done

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -8,7 +8,7 @@ const intArgs = scriptArgs.map(item => parseInt(item, 10));
 // console.log(intArgs);
 const sortedArgs = intArgs.sort((a, b) => b - a);
 const secondLargest = sortedArgs[1];
-if (args.length <= 2) {
+if (args.length <= 2 && args.length[3] === 1) {
   console.log(0);
 } else {
   console.log(secondLargest);
