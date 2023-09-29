69e747432f05dbd460461e0a6c1e9acc486e7af7
glenmiracle18 <m.bonyu@alustudent.com>
2023-09-29 15:31:16 +0200

    dn

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,14 +5,17 @@ for (let i = 2; i < arguments.length; i++) {
    scriptArgs.push(arguments[i]); 
 }
 const intArgs = scriptArgs.map(item => parseInt(item, 10));
-
+// console.log(intArgs);
 const sortedArgs = intArgs.sort();
-// console.log(intArgs)
-intArgs.pop();
+// console.log(sortedArgs)
+// intArgs.pop();
 const secondHighest = intArgs[intArgs.length - 1];
+console.log(secondHighest)
 
-if (arguments.length === 2) {
-  console.log(0);
-} else if (arguments[2] = 1) {
-    console.log(0);
-}
+// if (arguments.length === 2) {
+//   console.log(0);
+// } else if (arguments[2] = 1) {
+//     console.log(0);
+// } else {
+//     console.log(secondHighest)
+// }
