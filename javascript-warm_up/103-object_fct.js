1cf64849a5954eeeb17839d4f9280553292d53db
glenmiracle18 <m.bonyu@alustudent.com>
2023-09-30 16:29:34 +0200

    done

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+function incr () {
+  this.value++;
+}
+
+myObject.incr = incr.bind(myObject);
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
