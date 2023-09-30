d667d16f024b4896abbcca33a4889b5da222e947
glenmiracle18 <m.bonyu@alustudent.com>
2023-09-30 19:03:19 +0200

    practice

--- javascript-warm_up/js_practice/objects.js
+++ javascript-warm_up/js_practice/objects.js
@@ -3,6 +3,8 @@
 // console.log(dog.breed);
 // console.log(dog);
 
+// import { log } from "console";
+
 // dynamic typing
 // let myNumber = "500"; // oops, this is still a string
 // console.log(typeof(myNumber));
@@ -17,3 +19,80 @@
 // bird.species = "sparrow";
 // console.log(bird.species); // you can update the value of an object
 //                            // witht the const data type.
+
+// const person = {
+//     name: {
+//         first: "Bob",
+//         last: "Smith",
+//     },
+//     age: 32,
+//     bio() {
+//         console.log(
+//             `${this.name.first} ${this.name.last} is ${this.age} years old.`);
+//     },
+//     introduceSelf() {
+//         console.log(`Hi! I'm ${this.name.first}.`);
+//     },
+// };
+
+// person.bio()
+// person.introduceSelf()
+
+
+// const person = {
+//     name: ["Bob", "Smith"],
+//     age: 32,
+//     myDataName: "height",
+//     myDataValue: "1.9m",
+//     this[myDataName]: myDataValue,
+// };
+
+// function logProperty(propertyName) {
+//     console.log(person[propertyName]);
+// }
+
+// // person.age = 45;
+// // person["name"]["last"] = "Glen";
+// person['eyes'] = "blue";
+// person.farewell  = function () {
+//     console.log("Bye Everybody");
+// }
+
+// // console.dir(person);
+
+// logProperty("name");
+// logProperty("age");
+// person.farewell()
+// console.log(person.eyes);
+
+
+
+// const person1 = {
+//     name: "Glen",
+//     introduceSelf() {
+//         console.log(`Hi! I'm ${this.name}.`);
+//     },
+// };
+
+// const person2 = {
+//     name: "Miracle",
+//     introduceSelf () {
+//         console.log(`Hi! I'm ${this.name}.`);
+//     },
+// };
+
+// person1.introduceSelf()
+// person2.introduceSelf()
+
+
+
+function createPerson(name) {
+    const obj = {};
+    obj.name = name;
+    obj.introduceSelf = function () {
+        console.log(`Hi! I'm ${this.name}.`);
+    };
+    return obj;
+}
+createPerson('Glen').introduceSelf();
+console.log(createPerson('Glen').name);
