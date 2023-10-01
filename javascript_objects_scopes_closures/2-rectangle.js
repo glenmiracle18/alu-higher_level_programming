dc2ae6256d9b512d1e6096f327446fcd758fe227
glenmiracle18 <m.bonyu@alustudent.com>
2023-10-01 20:10:49 +0200

    done

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      return {};
+      return Object.create(Rectangle.prototype);
     } else {
       this.width = w;
       this.height = h;
