d590d0e31ff91fc4dc0c6fa7ffd83b5b1e70f117
glenmiracle18 <m.bonyu@alustudent.com>
2023-10-01 22:45:49 +0200

    done

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,21 +1,8 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function convertToBaseN (number) {
-    if (number < base) {
-    // For single-digit values, return the corresponding character if base is 16
-      return base === 16 && number >= 10
-        ? String.fromCharCode('A'.charCodeAt(0) + (number - 10))
-        : String(number);
-    } else {
-      const remainder = number % base;
-      return convertToBaseN(Math.floor(number / base)) + convertToBaseN(remainder);
-    }
+  if (base !== 10) {
+    return (a) => a.toString(base);
+  } else {
+    return (a) => parseInt(a, base);
   }
-
-  return function (number) {
-    if (base < 2 || base > 36) {
-      return null;
-    }
-    return convertToBaseN(number);
-  };
 };
