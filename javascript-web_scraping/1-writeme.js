5231ee8a8afb4d230364680c0db905d4630e7665
glenmiracle18 <m.bonyu@alustudent.com>
2023-10-04 23:00:31 +0200

    done

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const { error } = require('console');
 const fs = require('fs');
 const filepath = process.argv[2];
 const text = process.argv[3];
 
-fs.writeFile(filepath, text, (error) => {
-  if(error) {
-    console.error(error);
-  } 
-})
+try {
+  fs.writeFileSync(filepath, text);
+} catch (error) {
+  console.log(error);
+}
