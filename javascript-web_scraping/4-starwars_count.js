96d530b7f3538d393c31da58e8c7768fc0053af4
glenmiracle18 <m.bonyu@alustudent.com>
2023-10-05 02:02:49 +0200

    fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,46 @@
 #!/usr/bin/node
-const request = require('request');
-const apiUrl = process.argv[2];
-// const characterID =  'https://swapi-api.alx-tools.com/api/people/18/';
-let movieCount = 0;
+// const request = require('request');
+// const apiUrl = process.argv[2];
+// // const characterID =  'https://swapi-api.alx-tools.com/api/people/18/';
+// let movieCount = 0;
 
-request.get(apiUrl, (error, response, body) => {
+// request.get(apiUrl, (error, response, body) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     if (response.statusCode === 200) {
+//       const movie = JSON.parse(body);
+//       for (const film of movie.results) {
+//         if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+//           movieCount++;
+//         }
+//       }
+//       console.log(movieCount);
+//     } else {
+//       console.error(response.statusCode);
+//     }
+//   }
+// });
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
-    if (response.statusCode === 200) {
-      const movie = JSON.parse(body);
-      for (const film of movie.results) {
-        if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-          movieCount++;
+    const data = JSON.parse(body);
+    const movieCount = data.results.length;
+    let finalResult = 0;
+    // const comString = 'https://swapi-api.hbtn.io/api/people/18/';
+    for (let i = 0; i < movieCount; i++) {
+      const numChars = data.results[i].characters.length;
+      for (let j = 0; j < numChars; j++) {
+        const comString0 = data.results[i].characters[j];
+        if (comString0.slice(-4, -1) === '/18') {
+          finalResult += 1;
         }
       }
-      console.log(movieCount);
-    } else {
-      console.error(response.statusCode);
     }
+    console.log(finalResult);
   }
 });
