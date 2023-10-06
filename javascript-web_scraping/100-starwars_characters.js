af017ea21eb1bad5bc07c2352d9c9119f30304b8
glenmiracle18 <m.bonyu@alustudent.com>
2023-10-06 12:29:18 +0200

    done

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const movieID = process.argv[2];
+const request = require('request');
+const requestURL = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request(requestURL, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const movieIData = JSON.parse(body);
+
+    // console.log(movieIData.title);
+    movieIData.characters.forEach((characterURL1) => {
+      request.get(characterURL1, (charError, charaterResponse, charBody) => {
+        if (charError) {
+          console.error(error);
+        } else if (charaterResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  } else {
+    console.log(response.statusCode);
+  }
+});
