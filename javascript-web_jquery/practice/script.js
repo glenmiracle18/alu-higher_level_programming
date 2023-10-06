827b4d5705d4de1f49624570ff53a14eaa6da329
glenmiracle18 <m.bonyu@alustudent.com>
2023-10-06 17:40:59 +0200

    done

---
+++ javascript-web_jquery/practice/script.js
@@ -0,0 +1,20 @@
+// const button = document.querySelector("button");
+// button.addEventListener('click', updateName);
+
+// function updateName() {
+//  const name = prompt("Enter a new name");
+//  button.textContent = name;
+// }
+
+function createParagraph() {
+  const para = document.createElement("p");
+  para.textContent = `You clicked the button!`;
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
+}
+
